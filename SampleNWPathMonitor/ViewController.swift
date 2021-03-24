6b635fe914758bc4d55ee0ea3b64f4d853c75401
Miura Toya <dengzais8@gmail.com>
2021-03-24 22:48:12 +0900

    add

--- SampleNWPathMonitor/ViewController.swift
+++ SampleNWPathMonitor/ViewController.swift
@@ -8,21 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    private var isOnline: Bool? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
+        NetWorkManager.shared.isOnline { result in
+            self.isOnline = result
+        }
     }
     // 投稿ボタンタップ
     @IBAction func testBtn(_ sender: Any) {
+        guard let _isOnline = isOnline else { return }
+        print("DEBUG_PRINT:\(_isOnline)")
         // ネットワーク判定
-        NetWorkManager.shared.isOnline { result in
-            if result == true {
-                // オンライン時の処理
-                print("オンラインです")
-            } else {
-                // オフライン時の処理
-                print("オフラインです")
-            }
+        if _isOnline == true {
+            // オンライン時の処理
+            print("オンラインです")
+        } else {
+            // オフライン時の処理
+            print("オフラインです")
         }
     }
 }
